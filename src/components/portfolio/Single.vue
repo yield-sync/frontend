c19837a4334c94daec25ed2c03b713526b217202
Your Name <you@example.com>
2025-04-14 20:43:31 -0400

    Reformat portfolio

--- src/components/portfolio/Single.vue
+++ src/components/portfolio/Single.vue
@@ -2,24 +2,18 @@
 	<VContainer>
 		<VRow v-if="portfolio">
 			<VCol cols="12">
-				<h2 class="text-center">Portfolio: {{ portfolio.name }}</h2>
+				<h2 class="text-center">{{ portfolio.name }}</h2>
 			</VCol>
 
 			<VCol cols="12">
 				<VRow v-if="portfolioAssets.length > 0">
 					<VCol cols="12">
 						<VRow>
-							<VCol cols="2" lg="1" class="border border-light">
-								<h4 class="text-bold text-primary">Type</h4>
-							</VCol>
-							<VCol cols="2" lg="2" class="border border-light">
+							<VCol cols="6" lg="6">
 								<h4 class="text-bold text-primary">Symbol</h4>
 							</VCol>
-							<VCol cols="8" lg="7" class="border border-light">
-								<h4 class="text-bold text-primary">Name</h4>
-							</VCol>
 
-							<VCol cols="2" lg="2" class="border border-light">
+							<VCol cols="6" lg="6">
 								<h4 class="text-bold text-primary">% Allocation</h4>
 							</VCol>
 						</VRow>
@@ -27,18 +21,13 @@
 
 					<VCol v-for="a in portfolioAssets" :key="a.id" cols="12">
 						<VRow>
-							<VCol cols="2" lg="1" class="border border-light">
-								<h4 class="text-light">{{ a.cryptocurrency_symbol ? "Crypto" :"Stock" }}</h4>
-							</VCol>
-							<VCol cols="2" lg="2" class="border border-light">
-								<h4 class="text-light">{{ a.stock_symbol }}{{ a.cryptocurrency_symbol }}</h4>
-							</VCol>
-							<VCol cols="8" lg="7" class="border border-light">
-								<h4 class="text-light">{{ a.stock_name }}{{ a.cryptocurrency_name }}</h4>
+							<VCol cols="6" lg="6">
+								<h2 class="text-light">{{ a.stock_symbol }}{{ a.cryptocurrency_symbol }}</h2>
+								<h5 class="text-light">{{ a.stock_name }}{{ a.cryptocurrency_name }}</h5>
 							</VCol>
 
-							<VCol cols="2" lg="2" class="border border-light">
-								<h4 class="text-light">% {{ a.percent_allocation / 100 }}</h4>
+							<VCol cols="6" lg="6">
+								<h2 class="text-light">% {{ a.percent_allocation / 100 }}</h2>
 							</VCol>
 						</VRow>
 					</VCol>
