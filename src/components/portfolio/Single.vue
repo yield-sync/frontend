63cb8ae43f2abaad33e14311b3b095f93047b3f6
Your Name <you@example.com>
2025-04-19 20:46:16 -0400

    Better design

--- src/components/portfolio/Single.vue
+++ src/components/portfolio/Single.vue
@@ -1,15 +1,12 @@
 <template>
 	<VContainer style="max-width: 800px;">
-		<VRow v-if="portfolio">
-			<VCol cols="12">
-				<VCard
-					v-if="!updatePortfolioToggle"
-					class="mx-auto my-6 pa-6"
-					width="100%"
-					max-width="600"
-					elevation="0"
-					color="dark"
-				>
+		<div v-if="portfolio">
+			<VCard
+				width="100%"
+				elevation="0"
+				color="secondary"
+			>
+				<VCardTitle v-if="!updatePortfolioToggle">
 					<VRow>
 						<VCol cols="10">
 							<h2 class="text-light">{{ portfolio.name }}</h2>
@@ -33,52 +30,42 @@
 							</VMenu>
 						</VCol>
 					</VRow>
-				</VCard>
+				</VCardTitle>
 
-				<UpdateName
-					v-else
-					:id="id"
-					:name="portfolio.name"
-					:rules="[]"
-					:loading="loading"
-					@submit="onPortfolioNameUpdate"
-					@cancel="updatePortfolioToggle = false"
-				/>
-			</VCol>
+				<VCardText>
+					<UpdateName
+						v-if="updatePortfolioToggle"
+						:id="id"
+						:name="portfolio.name"
+						:rules="[]"
+						:loading="loading"
+						@submit="onPortfolioNameUpdate"
+						@cancel="updatePortfolioToggle = false"
+					/>
 
-			<VCol cols="12">
-				<VRow v-if="portfolioAssets.length > 0">
-					<VCol cols="12">
-						<VRow>
-							<VCol cols="6" lg="6">
-								<h4 class="text-bold text-primary">Symbol</h4>
-							</VCol>
+					<hr class="border border-dark my-3">
 
-							<VCol cols="6" lg="6">
-								<h4 class="text-bold text-primary">% Allocation</h4>
-							</VCol>
-						</VRow>
-					</VCol>
+					<VRow v-if="portfolioAssets.length > 0">
+						<VCol v-for="a in portfolioAssets" :key="a.id" cols="12">
+							<VRow>
+								<VCol cols="6" lg="6">
+									<h3 class="text-light">{{ a.stock_symbol }}{{ a.cryptocurrency_symbol }}</h3>
+									<h5 class="text-light">{{ a.stock_name }}{{ a.cryptocurrency_name }}</h5>
+								</VCol>
 
-					<VCol v-for="a in portfolioAssets" :key="a.id" cols="12">
-						<VRow>
-							<VCol cols="6" lg="6">
-								<h2 class="text-light">{{ a.stock_symbol }}{{ a.cryptocurrency_symbol }}</h2>
-								<h5 class="text-light">{{ a.stock_name }}{{ a.cryptocurrency_name }}</h5>
-							</VCol>
+								<VCol cols="6" lg="6">
+									<h3 class="text-right text-light">% {{ a.percent_allocation / 100 }}</h3>
+								</VCol>
+							</VRow>
+						</VCol>
+					</VRow>
+				</VCardText>
+			</VCard>
+		</div>
 
-							<VCol cols="6" lg="6">
-								<h2 class="text-light">% {{ a.percent_allocation / 100 }}</h2>
-							</VCol>
-						</VRow>
-					</VCol>
-				</VRow>
-
-				<div v-else>
-					<h3 class="text-center text-light">No portfolio assets</h3>
-				</div>
-			</VCol>
-		</VRow>
+		<div v-else>
+			<h3 class="text-center text-light">No portfolio assets</h3>
+		</div>
 
 		<h3 v-if="requestError" class="text-center text-error">{{ requestError }}</h3>
 
--- src/components/portfolio/UpdateName.vue
+++ src/components/portfolio/UpdateName.vue
@@ -1,54 +1,46 @@
 <template>
-	<VCard
-		class="mx-auto my-6 pa-6"
-		width="100%"
-		max-width="600"
-		elevation="0"
-		color="dark"
-	>
-		<VForm @submit.prevent="submit" validate-on="submit lazy" ref="formRef">
-			<VRow justify="center">
-				<VCol cols="12">
-					<VTextField
-						v-model="nameModel"
-						:rules="rules"
-						density="compact"
-						variant="outlined"
-						color="success"
-						label="Portfolio Name"
-						hide-details="auto"
-					/>
-				</VCol>
+	<VForm @submit.prevent="submit" validate-on="submit lazy" ref="formRef">
+		<VRow>
+			<VCol cols="12">
+				<VTextField
+					v-model="nameModel"
+					:rules="rules"
+					density="compact"
+					variant="outlined"
+					color="success"
+					label="Portfolio Name"
+					hide-details="auto"
+				/>
+			</VCol>
 
-				<VCol cols="6" class="text-center">
-					<VBtn
-						:loading="loading"
-						color="success"
-						variant="flat"
-						type="submit"
-						class="w-100"
-						size="small"
-						rounded
-					>
-						Save
-					</VBtn>
-				</VCol>
+			<VCol cols="6" class="text-center">
+				<VBtn
+					:loading="loading"
+					color="success"
+					variant="flat"
+					type="submit"
+					class="w-100"
+					size="small"
+					rounded
+				>
+					Save
+				</VBtn>
+			</VCol>
 
-				<VCol cols="6" class="text-center">
-					<VBtn
-						@click="$emit('cancel')"
-						color="grey-darken-2"
-						variant="outlined"
-						class="w-100"
-						size="small"
-						rounded
-					>
-						Cancel
-					</VBtn>
-				</VCol>
-			</VRow>
-		</VForm>
-	</VCard>
+			<VCol cols="6" class="text-center">
+				<VBtn
+					@click="$emit('cancel')"
+					color="grey-darken-2"
+					variant="outlined"
+					class="w-100"
+					size="small"
+					rounded
+				>
+					Cancel
+				</VBtn>
+			</VCol>
+		</VRow>
+	</VForm>
 </template>
 
 <script setup>
