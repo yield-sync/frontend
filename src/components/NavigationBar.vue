bbaea62b7db7efc1d86a370880631a8d912eb452
Your Name <you@example.com>
2025-04-25 17:06:20 -0400

    Add suggested drowndown menu

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -9,8 +9,10 @@
 
 			<VCol cols="12" sm="6">
 				<VForm @submit.prevent="search" validate-on="submit lazy" ref="formRef">
-					<VTextField
+					<VAutocomplete
 						v-model="query"
+						:items="suggestions"
+						:loading="loading"
 						:rules="rules"
 						variant="outlined"
 						color="light"
@@ -18,8 +20,12 @@
 						label="Ticker, Crypto, etc.."
 						append-inner-icon="mdi-magnify"
 						@click:append-inner="search"
+						@update:search="fetchSuggestions"
 						class="text-light mx-auto"
 						style="max-width: 600px;"
+						hide-no-data
+						hide-details
+						no-filter
 					/>
 				</VForm>
 			</VCol>
@@ -29,8 +35,7 @@
 					<VBtn color="primary" rounded elevation="0" class="w-100">Log In</VBtn>
 				</RouterLink>
 
-				<VBtn v-else @click="logOut"rounded color="secondary"
-					elevation="0" class="w-100">
+				<VBtn v-else @click="logOut" rounded color="secondary" elevation="0" class="w-100">
 					Log out
 				</VBtn>
 			</VCol>
@@ -38,10 +43,11 @@
 	</VContainer>
 </template>
 
-
 <script setup>
-	import { ref } from "vue";
+	import { ref, watch } from "vue";
+	import axios from "axios";
 	import { useRouter } from "vue-router";
+
 	import useAppStore from "@/stores/App";
 
 	const app = useAppStore();
@@ -49,17 +55,52 @@
 	const router = useRouter();
 
 	const query = ref("");
+	const suggestions = ref([]);
+	const loading = ref(false);
+
+	const rules = [(value) => !!value || "Query name is required"];
+
+	const URL = import.meta.env.MODE === "development"
+		? import.meta.env.VITE_DEV_SERVER_URL
+		: "";
+
+	const fetchSuggestions = async (input) => {
+		if (!input || input.length < 1) {
+			suggestions.value = [];
+			return;
+		}
+
+		loading.value = true;
+
+		try {
+
+			const authAxios = axios.create({
+				baseURL: `${URL}/api`,
+				headers: {
+					authorization: `Bearer ${localStorage.getItem("authToken")}`,
+				},
+			});
+
+			const res = await authAxios.get(`/stock/search/${input}`);
+
+			console.log(res);
+
+			suggestions.value = res.data?.stocks?.map((item) => item.symbol) ?? [];
 
 
-	const rules = [
-		(value) =>
+		}
+		catch (err)
 		{
-			return !!value || "Query name is required";
-		},
-	];
+			console.error("Error fetching suggestions:", err);
+			suggestions.value = [];
+		}
 
-	const search = async () =>
-	{
+		loading.value = false;
+	};
+
+	const search = async () => {
+		if (!query.value) return;
+
 		router.push(`/query/${query.value}`);
 
 		query.value = "";
@@ -73,4 +114,13 @@
 
 		app.setLoggedIn(false);
 	};
+
+	watch(query, (newVal, oldVal) =>
+	{
+		if (newVal && suggestions.value.includes(newVal))
+		{
+			router.push(`/stock/${newVal}`);
+			query.value = "";
+		}
+	});
 </script>
