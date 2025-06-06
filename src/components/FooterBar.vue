0433bd4358f1e4a97403f8bdc8c9be646fc20067
Your Name <you@example.com>
2025-06-06 17:44:51 -0400

    simplify footer

--- src/components/FooterBar.vue
+++ src/components/FooterBar.vue
@@ -1,13 +1,4 @@
 <template>
-	<div style="background-attachment: fixed; background-size: cover;">
-		<VSheet color="transparent">
-			<VContainer>
-				<VRow>
-					<VCol cols="12" lg="6"/>
-				</VRow>
-			</VContainer>
-		</VSheet>
-	</div>
 </template>
 
 <script setup>
